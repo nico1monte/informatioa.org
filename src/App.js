87420945f5ee641bf903ad38e97e2578208d51bc
arsenii10 <arseniisokolov12@gmail.com>
2025-06-22 21:28:33 -0300

    add header

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
+import ProfileIcon from './icons/profile.svg';
+import ContactsIcon from './icons/contacts.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={'/logo.jpg'} className="App-logo" alt="logo" />
+        <h1 className="App-header-logo-text">
+          Informatioa
+        </h1>
+        <button type="button" className="App-header-button App-header-profile">
+          <ProfileIcon />
+        </button>
+        <button type="button" className="App-header-button App-header-contacts">
+          <ContactsIcon />
+        </button>
+        <div className="App-header-search">
+          <input type="text" placeholder="Search Projects" />
+        </div>
       </header>
     </div>
   );
